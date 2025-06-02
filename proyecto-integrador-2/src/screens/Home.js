f83dad223f4d23d9972b48d1786754340a8f0d15
JoacoRosas <jrosaszuiani@udesa.edu.ar>
2025-06-02 12:13:21 -0300

    estructura likeador(falta completar)

--- proyecto-integrador-2/src/screens/Home.js
+++ proyecto-integrador-2/src/screens/Home.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, TouchableOpacity,StyleSheet } from 'react-native'
 import {auth, db} from '../firebase/config'
+import firebase from 'firebase'
 
 export default class Home extends Component {
   constructor(props){
     super(props)
     this.state={
         posteos: [],
-        loading: true
+        loading: true,
+        cantMeGusta: 0,
+        likes: false
     }
   }
 
@@ -23,12 +26,56 @@ export default class Home extends Component {
         }, () => console.log('El state (los posteos)', this.state)) ////este segundo parametro es oara ver que quedo en el estado (los posteos que hay)
     })
 }
+
+  likearPost(){
+      db.collection('posts')
+      .doc(this.props.id)
+      .update({
+        likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
+      })
+      .then(() => this.setState({
+        meGusta: true,
+        cantMeGusta: this.state.cantMeGusta + 1
+      }))
+    }
+
+    unlikePost(){
+      db.collection('posts')
+      .doc(this.props.id)
+      .update({
+        likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+      })
+      .then(() => this.setState({
+        meGusta: false,
+        cantMeGusta: this.state.cantMeGusta - 1
+      }))
+    }
  //La lista de los posteos ya esta creada en orden de los mas recientes primero, ahora hay que mostralo en el return con un .map() y que se renderice uno por uno con un componente de Post.js
   render() {
     return (
       <View>
         <Text style={styles.titulo}> Snapgram </Text>
-        
+        <Text>Cantidad de me gusta: {this.state.cantMeGusta}</Text>
+       
+        {
+            this.state.meGusta ?
+            <TouchableOpacity
+                onPress={() => this.likearPost()}
+            >
+                <Text>
+                    Me gusta
+                </Text>
+            </TouchableOpacity>
+            :
+            <TouchableOpacity
+                onPress={() => this.unlikePost()}
+            >
+                <Text>
+                   Me gusta
+                </Text>
+            </TouchableOpacity>
+        }
+             
       </View>
     )
   }
