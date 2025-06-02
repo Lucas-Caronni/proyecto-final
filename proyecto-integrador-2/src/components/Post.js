77b5c738a01d8430948ad94516b41429d03b9ae7
JoacoRosas <jrosaszuiani@udesa.edu.ar>
2025-06-02 15:46:11 -0300

    corregí el likear posteos

--- proyecto-integrador-2/src/components/Post.js
+++ proyecto-integrador-2/src/components/Post.js
@@ -1,27 +1,83 @@
-import { View, Text, TouchableOpacity, FlatList } from 'react-native'
-import React, {Component} from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { Component } from 'react'
 import { auth, db } from '../firebase/config'
+import firebase from 'firebase'
 
 export default class Post extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            meGusta: false,
+            cantMeGusta: this.props.data.likes? this.props.data.likes.length : 0,
         }
     }
-    borrarPost(){
-        db.collection('posts').doc(this.props.id).delete()
-        .then(() => console.log('Eliminado'))
-        .catch(error => console.log(error))
+
+    componentDidMount() {
+        if (this.props.data.likes) {
+            const meGusta = this.props.data.likes.includes(auth.currentUser.email)
+            this.setState({
+                meGusta: meGusta
+            })
+        }
     }
 
-    render(){
-        return(
+    likearPost() {
+        db.collection('posts')
+            .doc(this.props.id)
+            .update({
+                likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
+            })
+            .then(() => this.setState({
+                meGusta: true,
+                cantMeGusta: this.state.cantMeGusta + 1
+            }))
+    }
+
+    unlikePost() {
+        db.collection('posts')
+            .doc(this.props.id)
+            .update({
+                likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+            })
+            .then(() => this.setState({
+                meGusta: false,
+                cantMeGusta: this.state.cantMeGusta - 1
+            }))
+    }
+
+    borrarPost() {
+        db.collection('posts').doc(this.props.id).delete()
+            .then(() => console.log('Eliminado'))
+            .catch(error => console.log(error))
+    }
+
+    render() {
+        return (
             <View>
-                <Text>{this.props.data.post}</Text>
                 <Text>{this.props.data.description}</Text>
-                <TouchableOpacity onPress={() => this.borrarPost()}>
-                    <Text>Borrar posteos</Text>
-                </TouchableOpacity>
+                <Text>{this.props.data.owner}</Text>
+
+                <Text>Cantidad de me gusta: {this.state.cantMeGusta}</Text>
+
+                {
+                    this.state.meGusta ?
+                        <TouchableOpacity onPress={() => this.unlikePost()}>
+                            <Text>Ya no me gusta</Text>
+                        </TouchableOpacity>
+                        :
+                        <TouchableOpacity onPress={() => this.likearPost()}>
+                            <Text>Me gusta</Text>
+                        </TouchableOpacity>
+                }
+
+                {/* Mostrar botón de borrar solo si el post es del usuario actual */}
+                {
+                    this.props.data.owner === auth.currentUser.email ?
+                        <TouchableOpacity onPress={() => this.borrarPost()}>
+                            <Text>Borrar posteo</Text>
+                        </TouchableOpacity>
+                        : null
+                }
             </View>
         )
     }
--- proyecto-integrador-2/src/screens/Home.js
+++ proyecto-integrador-2/src/screens/Home.js
@@ -1,89 +1,47 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity,StyleSheet } from 'react-native'
-import {auth, db} from '../firebase/config'
-import firebase from 'firebase'
+import { Text, View, StyleSheet, FlatList } from 'react-native'
+import { db } from '../firebase/config'
+import Post from '../components/Post'
 
 export default class Home extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={
-        posteos: [],
-        loading: true,
-        cantMeGusta: 0,
-        likes: false
+    this.state = {
+      posteos: []
     }
   }
 
-  componentDidMount(){
-    db.collection('posts').orderBy('createdAt', 'desc').onSnapshot((docs)=>{
-        let posts = [];
-        docs.forEach((doc)=> posts.push({
-            id: doc.id,
-            data: doc.data()
-        }))
-        this.setState({
-            posteos: posts
-        }, () => console.log('El state (los posteos)', this.state)) ////este segundo parametro es oara ver que quedo en el estado (los posteos que hay)
-    })
-}
-
-  likearPost(){
-      db.collection('posts')
-      .doc(this.props.id)
-      .update({
-        likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
-      })
-      .then(() => this.setState({
-        meGusta: true,
-        cantMeGusta: this.state.cantMeGusta + 1
+  componentDidMount() {
+    db.collection('posts').orderBy('createdAt', 'desc').onSnapshot((docs) => {
+      let posts = [];
+      docs.forEach((doc) => posts.push({
+        id: doc.id,
+        data: doc.data()
       }))
-    }
-
-    unlikePost(){
-      db.collection('posts')
-      .doc(this.props.id)
-      .update({
-        likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+      this.setState({
+        posteos: posts
       })
-      .then(() => this.setState({
-        meGusta: false,
-        cantMeGusta: this.state.cantMeGusta - 1
-      }))
-    }
- //La lista de los posteos ya esta creada en orden de los mas recientes primero, ahora hay que mostralo en el return con un .map() y que se renderice uno por uno con un componente de Post.js
+    }),() => console.log('El state (los posteos)', this.state) ////este segundo parametro es oara ver que quedo en el estado (los posteos que hay)
+  }
+
+//La lista de los posteos ya esta creada en orden de los mas recientes primero, ahora hay que mostralo en el return con un .map() y que se renderice uno por uno con un componente de Post.js
   render() {
     return (
       <View>
-        <Text style={styles.titulo}> Snapgram </Text>
-        <Text>Cantidad de me gusta: {this.state.cantMeGusta}</Text>
-       
-        {
-            this.state.meGusta ?
-            <TouchableOpacity
-                onPress={() => this.likearPost()}
-            >
-                <Text>
-                    Me gusta
-                </Text>
-            </TouchableOpacity>
-            :
-            <TouchableOpacity
-                onPress={() => this.unlikePost()}
-            >
-                <Text>
-                   Me gusta
-                </Text>
-            </TouchableOpacity>
-        }
-             
+        <Text style={styles.titulo}>Snapgram</Text>
+        <FlatList
+          data={this.state.posteos}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => <Post id={item.id} data={item.data} />}
+        />
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-    titulo: {
-        fontSize: 32,
-        textAlign: 'center'
-    }
+  titulo: {
+    fontSize: 32,
+    textAlign: 'center'
+  }
 })
